6318616cee03b39093083beaac8a562d072bb27f
vishnuchandrappan <cvishnu70@gmail.com>
2019-12-23 19:35:52 +0530

    added signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get '/about', to: 'static_pages#about'
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'static_pages#home'
+  get '/about', to: 'static_pages#about'
+
+  get '/signup', to: 'users#new'
+
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,14 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = " | Rails Blog"
+  end
+
   test "should get new" do
-    get users_new_url
+    get signup_url
     assert_response :success
+    assert_select "title", "SignUp#{@base_title}"
   end
 
 end
