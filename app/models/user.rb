659246de980197503e29d7948f80ea9ebece69d1
vishnuchandrappan <cvishnu70@gmail.com>
2019-12-24 19:39:34 +0530

    add user model tests

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d]+([\-.][a-z]+)*\.[a-z]+\z/i.freeze
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+
+  has_secure_password
+end
