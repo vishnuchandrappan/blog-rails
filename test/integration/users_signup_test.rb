f37a2bff70cd8b52f4a78bf7d5ce151286f1953b
vishnuchandrappan <cvishnu70@gmail.com>
2019-12-24 22:31:54 +0530

    SSL & puma config

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,16 +1,17 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-
   test 'invalid signup' do
     get signup_path
     assert_no_difference 'User.count' do
       post users_path, params: {
         user: {
-          name: "",
-          email: "user@invalid",
-          password: "foo",
-          password_confirmation: "bar"
+          name: '',
+          email: 'user@invalid',
+          password: 'foo',
+          password_confirmation: 'bar'
         }
       }
     end
@@ -22,10 +23,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_difference 'User.count' do
       post users_path, params: {
         user: {
-          name: "John Doe",
-          email: "john@example.com",
-          password: "foobar",
-          password_confirmation: "foobar"
+          name: 'John Doe',
+          email: 'john@example.com',
+          password: 'foobar',
+          password_confirmation: 'foobar'
         }
       }
     end
@@ -33,5 +34,4 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/show'
     assert_not flash.empty?
   end
-
 end
