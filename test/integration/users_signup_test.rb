4ad2859240fba6ee4a3774e5ad983dd2a617d702
vishnuchandrappan <cvishnu70@gmail.com>
2019-12-24 21:45:01 +0530

    add user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,36 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test 'invalid signup' do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: {
+        user: {
+          name: "",
+          email: "user@invalid",
+          password: "foo",
+          password_confirmation: "bar"
+        }
+      }
+    end
+    assert_template 'users/new'
+  end
+
+  test 'valid signup' do
+    get signup_path
+    assert_difference 'User.count' do
+      post users_path, params: {
+        user: {
+          name: "John Doe",
+          email: "john@example.com",
+          password: "foobar",
+          password_confirmation: "foobar"
+        }
+      }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+  end
+
+end
