6e9b46a2ccb5d10b6c9b4a554dd4a41fd9f19dbb
vishnuchandrappan <cvishnu70@gmail.com>
2019-12-31 15:13:17 +0530

    added pagination tests

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'puma', '~> 3.7'
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
+gem 'will_paginate',           '3.1.6'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
---
+++ test/integration/users_index_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class UsersIndexTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:vishnu)
+  end
+
+  test "index" do
+    log_in_as(@user)
+    get users_path
+    assert_template 'users/index'
+    assert_select 'div.pagination'
+    User.paginate(page: 1).each do |user|
+      assert_select 'a[href=?]', user_path(user), text: user.name unless user == @user
+    end
+  end
+
+end
