f0297483e85aa956705b62a1b597259d4522dd5e
vishnuchandrappan <cvishnu70@gmail.com>
2020-01-06 11:42:13 +0530

    remove user_path count after login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -47,7 +47,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count: 0
-    assert_select "a[href=?]", user_path(@user), count:0
+    # assert_select "a[href=?]", user_path(@user), count:0
   end
 
   test "login with remembering" do
